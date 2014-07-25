d2811cca7dd10c157ff9e7886c6e6b08395e6f3a
andrepitombeira <andrepitombeira@gmail.com>
2014-07-25 23:32:31 +0200

    Generate topics objects and pick only label and sentimentScore attributes

--- app/controllers/topics.js
+++ app/controllers/topics.js
@@ -1,5 +1,22 @@
+/*global _:true*/
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+    generateTopics: function() {
+        var topics = this.get("content.content");
 
+        topics = _.map(topics, function(topic) {
+            return Ember.Object.create(topic.get('data'));
+        });
+
+        return topics;
+    },
+
+    topics: function() {
+        var topics = this.generateTopics().map(function(topic) {
+            return _.pick(topic, ["label","sentimentScore"]);
+        });
+
+        return topics;
+    }.property('@each.label', '@each.sentimentScore')
 });
