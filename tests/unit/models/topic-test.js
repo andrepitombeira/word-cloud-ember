59bf5c6706b7d97869d8563b5ba35d76456cbe13
andrepitombeira <andrepitombeira@gmail.com>
2014-07-28 22:26:00 +0200

    Add unit tests for topic model

--- tests/unit/models/topic-test.js
+++ tests/unit/models/topic-test.js
@@ -1,4 +1,5 @@
 import { test, moduleForModel } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('topic', 'Topic', {
   // Specify the other units that are required for this test.
@@ -7,6 +8,18 @@ moduleForModel('topic', 'Topic', {
 
 test('it exists', function() {
   var model = this.subject();
-  // var store = this.store();
+
   ok(model);
+});
+
+test('promise', function () {
+    return Ember.run(function() {
+        return new Ember.RSVP.Promise(function(resolve) {
+            Ember.run.later(function(){
+
+                ok(true, "correct");
+                resolve("ok");
+            }, 1000);
+        });
+    });
 });
