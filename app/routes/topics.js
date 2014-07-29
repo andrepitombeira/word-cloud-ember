1dc8e4b247620baf1ac616ae13e3320ef5b908f6
André Pitombeira <andre.pitombeira@mantro.net>
2014-07-29 16:42:57 +0200

    Create documentation

--- app/routes/topics.js
+++ app/routes/topics.js
@@ -1,6 +1,16 @@
 import Ember from 'ember';
 
+/**
+ * Route for topic
+ * @class TopicRoute
+ */
 export default Ember.Route.extend({
+
+    /**
+     * Load topics from topic fixtures
+     * @method model
+     * @returns topics
+     */
     model: function() {
         return this.store.find('topic');
     }
