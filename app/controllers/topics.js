74aafeb9cc97b2436df2df0d27e94552e9e33a4b
André Pitombeira <andre.pitombeira@mantro.net>
2014-07-29 13:19:20 +0200

    Add documentation

--- app/controllers/topics.js
+++ app/controllers/topics.js
@@ -1,18 +1,32 @@
 /*global _:true*/
 import Ember from 'ember';
 
+/**
+ * This class is responsible for handling topics model
+ * @class Topics controller
+ */
 export default Ember.ArrayController.extend({
     selectedTopic: null,
 
+    /**
+     * Topic selected on the word cloud by user. This property is used in topic details component
+     * @property topic
+     * @type {Object}
+     */
     topic: function() {
         return this.get('selectedTopic');
     }.property('selectedTopic'),
 
+    /**
+     * Load the topics from fixtures and transform each item in a ember object.
+     * @method generateTopics
+     * @return {Array} Returns topics
+     */
     generateTopics: function() {
         var topics = this.get('content.content');
 
         if (!topics || _.isEmpty(topics)) {
-            return;
+            return topics;
         }
 
         topics = _.map(topics, function(topic) {
@@ -22,6 +36,11 @@ export default Ember.ArrayController.extend({
         return topics;
     },
 
+    /**
+     * Topics load from fixtures
+     * @property topics
+     * @type {Object} Returns topics
+     */
     topics: function() {
         var topics = this.generateTopics();
 
@@ -35,6 +54,11 @@ export default Ember.ArrayController.extend({
     }.property('@each.label', '@each.sentimentScore', '@each.volume', '@each.sentiment'),
 
     actions: {
+        /**
+         * Set selected topic
+         * @method setSelectedTopic
+         * @param {Object} selected topic
+         */
         setSelectedTopic: function(topic) {
             this.set('selectedTopic', topic);
         }
